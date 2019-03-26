e29556eadd1f59adbac4543b838cfd59258bee53
Alistair King <alistair@caida.org>
2019-03-26 10:21:02 -0700

    License under BSD license

---
+++ README.md
@@ -0,0 +1,44 @@
+# PyTimeSeries
+
+PyTimeSeries is a Python library that provides a high-performance
+abstraction layer for efficiently writing to time series databases. It
+is optimized for writing values for many time series simultaneously.
+
+PyTimeSeries uses the
+[libtimeseries](https://github.com/caida/libtimeseries) C library.
+
+## Quick Start
+
+To get started using PyTimeSeries, first
+[install libtimeseries](https://github.com/caida/libtimeseries).
+
+Then, you should be able to install PyTimeSeries.
+
+### Installing PyIPMeta from source
+
+```
+$ git clone https://github.com/caida/pyipmeta.git
+$ cd pyipmeta
+$ python setup.py build_ext
+$ sudo python setup.py install
+```
+
+Use python setup.py install --user to install the library in your home directory.
+
+## Usage
+
+See [test/_pytimeseries_test.py](/test/_pytimeseries_test.py) for a
+working example of how to use PyTimeSeries.
+
+## Copyright and Open Source Software
+
+Unless otherwise specified (below or in file headers) PyTimeseries is
+Copyright The Regents of the University of California and released
+under a BSD license. See the [LICENSE](LICENSE) file for details.
+
+### External Dependencies
+
+#### Required
+
+ - [libtimeseries](https://github.com/caida/libtimeseries) is [released
+   under BSD and compatible licenses](https://github.com/caida/libtimeseries#copyright-and-open-source-software).
--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
+#
+# Copyright (C) 2017 The Regents of the University of California.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
 from setuptools import setup, Extension, find_packages
 
 _pytimeseries_module = Extension("_pytimeseries",
@@ -9,11 +35,11 @@ _pytimeseries_module = Extension("_pytimeseries",
 
 setup(name="pytimeseries",
       description="A Python interface to libtimeseries",
-      version="0.2.0",
+      version="1.0.0",
       author="Alistair King",
       author_email="corsaro-info@caida.org",
       url="http://github.com/CAIDA/pytimeseries",
-      license="GPLv3",
+      license="BSD",
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
@@ -21,7 +47,6 @@ setup(name="pytimeseries",
           'Intended Audience :: System Administrators',
           'Intended Audience :: Telecommunications Industry',
           'Intended Audience :: Information Technology',
-          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Operating System :: POSIX',
       ],
       keywords='_pytimeseries pytimeseries timeseries graphite',
